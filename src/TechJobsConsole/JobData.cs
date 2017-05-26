c439f8fd28e94205d8570e507f03c2d42ebbdae0
andy <andy.russell.code@gmail.com>
2017-05-26 11:29:06 -0500

    TechJobsCOnsole ready to turn in.

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -1,55 +1,57 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using System.Text;
+﻿using System.Collections.Generic;           //classes/methods for data collections
+using System.IO;                            //classes/methods for reading and writing to other files
+using System.Reflection;                    //classes/methods for ?????????
+using System.Text;                          //classes/methods for dealing with ASCII char and strings
 
 namespace TechJobsConsole
 {
-    class JobData
+    class JobData                                                   //class that contains all text in file
     {
-        static List<Dictionary<string, string>> AllJobs = new List<Dictionary<string, string>>();
-        static bool IsDataLoaded = false;
+        static List<Dictionary<string, string>> AllJobs = new List<Dictionary<string, string>>();   //List of dict where each is a job...static b/c it refers to entire class
+        static bool IsDataLoaded = false;                           //bool object set to false
 
-        public static List<Dictionary<string, string>> FindAll()
+        public static List<Dictionary<string, string>> FindAll()    //method that can be seen outside the JobData class and exists identically for all instances    
         {
-            LoadData();
-            return AllJobs;
+            LoadData();                                                 //execute LoadData method
+            return AllJobs;                                             
         }
 
         /*
          * Returns a list of all values contained in a given column,
          * without duplicates. 
          */
-        public static List<string> FindAll(string column)
+        public static List<string> FindAll(string column)           //Method that takes string and returns a list of strings for all jobs in a column
         {
-            LoadData();
+            LoadData();                                                 //Call LoadData func
 
-            List<string> values = new List<string>();
+            List<string> values = new List<string>();                   //New list with string values
 
-            foreach (Dictionary<string, string> job in AllJobs)
+            foreach (Dictionary<string, string> job in AllJobs)         //for each job in AllJobs
             {
-                string aValue = job[column];
+                string aValue = job[column];                                //New string that contains the value for a certain row of a  certain column
 
-                if (!values.Contains(aValue))
+                if (!values.Contains(aValue))                               //If values does not contain aValue...
                 {
-                    values.Add(aValue);
+                    values.Add(aValue);                                         //add aValue to values
                 }
             }
-            return values;
+            return values;                                              //return values
         }
 
-        public static List<Dictionary<string, string>> FindByColumnAndValue(string column, string value)
+        public static List<Dictionary<string, string>> FindByColumnAndValue(string column, string value)    //??????????????????????
         {
             // load data, if not already loaded
             LoadData();
 
-            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>(); //Create list of dict jobs
 
-            foreach (Dictionary<string, string> row in AllJobs)
+            foreach (Dictionary<string, string> row in AllJobs)     //for each dict in list
             {
-                string aValue = row[column];
-
-                if (aValue.Contains(value))
+                string aValue = row[column];                            //new string with value of a certain column and certain row
+                string upperaValue = aValue.ToUpper();
+                string upperValue = value.ToUpper();
+                
+                if (upperaValue.Contains(upperValue))                             
                 {
                     jobs.Add(row);
                 }
@@ -61,82 +63,108 @@ namespace TechJobsConsole
         /*
          * Load and parse data from job_data.csv
          */
-        private static void LoadData()
+        private static void LoadData()                              //Method to load and parse data from the .CSV
         {
 
-            if (IsDataLoaded)
+            if (IsDataLoaded)                                           //Bool from line 11...Return if true 
             {
                 return;
             }
 
-            List<string[]> rows = new List<string[]>();
+            List<string[]> rows = new List<string[]>();                 //Create a list of string arrays where each array of strings is a row.
 
-            using (StreamReader reader = File.OpenText("job_data.csv"))
+            using (StreamReader reader = File.OpenText("job_data.csv")) //Use some outside method to access the .CSV??????????????????
             {
-                while (reader.Peek() >= 0)
+                while (reader.Peek() >= 0)                                  //Use .Peek to check for end of .CSV file        
                 {
-                    string line = reader.ReadLine();
-                    string[] rowArrray = CSVRowToStringArray(line);
-                    if (rowArrray.Length > 0)
-                    {
-                        rows.Add(rowArrray);
+                    string line = reader.ReadLine();                            //Create a string of char read from the .CSV
+                    string[] rowArrray = CSVRowToStringArray(line);             //Use method from 108 to parse CSV row to string array
+                    if (rowArrray.Length > 0)                                   //If the array has any char...
+                    {                   
+                        rows.Add(rowArrray);                                        //...then add it to rows
                     }
                 }
             }
 
-            string[] headers = rows[0];
-            rows.Remove(headers);
+            string[] headers = rows[0];                                 //Create string array headers that contains the first line of rows????????????
+            rows.Remove(headers);                                       //Remove first line (headers) from rows???????????????
 
             // Parse each row array into a more friendly Dictionary
-            foreach (string[] row in rows)
+            foreach (string[] row in rows)                              //For each string array in rows...
             {
-                Dictionary<string, string> rowDict = new Dictionary<string, string>();
+                Dictionary<string, string> rowDict = new Dictionary<string, string>();  //Create a dict (for each row) where K and V are strings
 
-                for (int i = 0; i < headers.Length; i++)
+                for (int i = 0; i < headers.Length; i++)                    //Iterate through each heading in headers
                 {
-                    rowDict.Add(headers[i], row[i]);
+                    rowDict.Add(headers[i], row[i]);                            //NOT WORKING DOWN ROWS...Pairs each heading KEY with VALUE data from that row 
                 }
-                AllJobs.Add(rowDict);
+                AllJobs.Add(rowDict);                                       //Add this dict of single job to AllJobs list
             }
 
-            IsDataLoaded = true;
+            IsDataLoaded = true;                                        //After AllJobs is created, set IsDataLoaded to true
         }
 
         /*
          * Parse a single line of a CSV file into a string array
          */
-        private static string[] CSVRowToStringArray(string row, char fieldSeparator = ',', char stringSeparator = '\"')
+        private static string[] CSVRowToStringArray(string row, char fieldSeparator = ',', char stringSeparator = '\"') //method takes CSV row and separates by "," and creates string array
         {
-            bool isBetweenQuotes = false;
-            StringBuilder valueBuilder = new StringBuilder();
-            List<string> rowValues = new List<string>();
+            bool isBetweenQuotes = false;                               //Bool set to false
+            StringBuilder valueBuilder = new StringBuilder();           //Declare new instance of SB called valueBuilder
+            List<string> rowValues = new List<string>();                //Create a list of strings rowValue
 
             // Loop through the row string one char at a time
-            foreach (char c in row.ToCharArray())
+            foreach (char c in row.ToCharArray())                       //For each char in an array made from row
             {
-                if ((c == fieldSeparator && !isBetweenQuotes))
+                if ((c == fieldSeparator && !isBetweenQuotes))              //If the char is , and not between quotes...
                 {
-                    rowValues.Add(valueBuilder.ToString());
-                    valueBuilder.Clear();
+                    rowValues.Add(valueBuilder.ToString());                     //Add a row number???????????????????
+                    valueBuilder.Clear();                                       //Remove all char from the StringBuilder
                 }
-                else
+                else                                                        //if not...
                 {
-                    if (c == stringSeparator)
+                    if (c == stringSeparator)                                   //if char == string separator (but not in quotes)
                     {
-                        isBetweenQuotes = !isBetweenQuotes;
+                        isBetweenQuotes = !isBetweenQuotes;                         //??????Set bool to false or opposite of what it was
                     }
-                    else
+                    else                                                        //if char not string separator...
                     {
-                        valueBuilder.Append(c);
+                        valueBuilder.Append(c);                                     //Add the char to the string
                     }
                 }
             }
 
             // Add the final value
-            rowValues.Add(valueBuilder.ToString());
-            valueBuilder.Clear();
+            rowValues.Add(valueBuilder.ToString());                     //Add the StringBuilder string to rowValues list
+            valueBuilder.Clear();                                       //Clear the StringBuilder
 
-            return rowValues.ToArray();
+            return rowValues.ToArray();                                 //return an array version of the list????????
         }
-    }
+
+        public static List<Dictionary<string, string>> FindByValue(string value)    //
+        {
+            // load data, if not already loaded
+            LoadData();
+
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>(); //Create list of dict jobs
+
+            foreach (Dictionary<string, string> row in AllJobs)     //for each dict in list
+            {
+
+                foreach (KeyValuePair<string, string> criteria in row)
+                {  //for each KVP in the dict
+
+                    string upperCriteria = criteria.Value.ToUpper();
+                    string upperValue = value.ToUpper();
+
+                    if (upperCriteria.Contains(upperValue))                               //if value matches the dict value
+                    {
+                        jobs.Add(row);                                          //add the jobs to jobs list<dict..>                                                                                     
+                    }
+                }
+            }
+
+            return jobs;
+        }
+    }   
 }
--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -1,74 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TechJobsConsole
 {
-    class Program
+    class Program                   //All code falls within classes in C# because it is object-oriented.
     {
-        static void Main(string[] args)
+        static void Main(string[] args)     
         {
             // Create two Dictionary vars to hold info for menu and data
 
             // Top-level menu options
-            Dictionary<string, string> actionChoices = new Dictionary<string, string>();
-            actionChoices.Add("search", "Search");
-            actionChoices.Add("list", "List");
+            Dictionary<string, string> actionChoices = new Dictionary<string, string>();    //Dict "actionChoices" where both key and value are strings
+            actionChoices.Add("search", "Search");                                          //add key "search" with value "Search"
+            actionChoices.Add("list", "List");                                              //add key "list" with value "List
 
             // Column options
-            Dictionary<string, string> columnChoices = new Dictionary<string, string>();
-            columnChoices.Add("core competency", "Skill");
+            Dictionary<string, string> columnChoices = new Dictionary<string, string>();    //Dict "columnChoices" where both key and value will be strings
+            columnChoices.Add("core competency", "Skill");                                  //Add key/value pairs to columnChoices
             columnChoices.Add("employer", "Employer");
             columnChoices.Add("location", "Location");
             columnChoices.Add("position type", "Position Type");
             columnChoices.Add("all", "All");
 
-            Console.WriteLine("Welcome to LaunchCode's TechJobs App!");
+            Console.WriteLine("Welcome to LaunchCode's TechJobs App!");                     //Welcome the user
 
             // Allow user to search/list until they manually quit with ctrl+c
-            while (true)
+            while (true)                                                                    //repeat all these steps until user kills prog
             {
 
-                string actionChoice = GetUserSelection("View Jobs", actionChoices);
+                string actionChoice = GetUserSelection("View Jobs", actionChoices);             //create string with call to GUS method and pass in "View Jobs" and actionChoices
 
-                if (actionChoice.Equals("list"))
+                if (actionChoice.Equals("list"))                                                //if user chooses list...
                 {
-                    string columnChoice = GetUserSelection("List", columnChoices);
+                    string columnChoice = GetUserSelection("List", columnChoices);                  //Create string with GUS call using list and their choice of column
 
-                    if (columnChoice.Equals("all"))
+                    if (columnChoice.Equals("all"))                                                 //if they choose all...
                     {
-                        PrintJobs(JobData.FindAll());
+                        PrintJobs(JobData.FindAll());                                                   //print list of everything
                     }
-                    else
-                    {
-                        List<string> results = JobData.FindAll(columnChoice);
+                    else                                                                            //if not...
+                                        {
+                        List<string> results = JobData.FindAll(columnChoice);                           //create strList results with list of desired column 
 
-                        Console.WriteLine("\n*** All " + columnChoices[columnChoice] + " Values ***");
-                        foreach (string item in results)
+                        Console.WriteLine("\n*** All " + columnChoices[columnChoice] + " Values ***");  //Print heading for list
+                        foreach (string item in results)                                                //For each string in results
                         {
-                            Console.WriteLine(item);
+                            Console.WriteLine(item);                                                    //Print it to console
                         }
                     }
                 }
-                else // choice is "search"
+                else // choice is "search"                                                      //if not, then the choice is "search"...
                 {
                     // How does the user want to search (e.g. by skill or employer)
-                    string columnChoice = GetUserSelection("Search", columnChoices);
+                    string columnChoice = GetUserSelection("Search", columnChoices);                //str columnChoice 
 
-                    // What is their search term?
-                    Console.WriteLine("\nSearch term: ");
-                    string searchTerm = Console.ReadLine();
+                    // What is their search term?                                                   
+                    Console.WriteLine("\nSearch term: ");                                           //request user search term
+                    string searchTerm = Console.ReadLine();                                         //retrieve user search term
 
-                    List<Dictionary<string, string>> searchResults;
+                    List<Dictionary<string, string>> searchResults;                                 //Declare list searchResults where each item is a dict
 
                     // Fetch results
-                    if (columnChoice.Equals("all"))
+                    if (columnChoice.Equals("all"))                                                 //if user picked all...
                     {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                        searchResults = JobData.FindByValue(searchTerm);     //Initialize searchResults list with method from JobData file user columnChoice and searchTerm as args
+                        PrintJobs(searchResults);                                          
                     }
-                    else
+                    else                                                                            //if user picked something other than all...
                     {
-                        searchResults = JobData.FindByColumnAndValue(columnChoice, searchTerm);
-                        PrintJobs(searchResults);
+                        searchResults = JobData.FindByColumnAndValue(columnChoice, searchTerm);    
+                        PrintJobs(searchResults);                                                   
                     }
                 }
             }
@@ -77,48 +79,70 @@ namespace TechJobsConsole
         /*
          * Returns the key of the selected item from the choices Dictionary
          */
-        private static string GetUserSelection(string choiceHeader, Dictionary<string, string> choices)
+        private static string GetUserSelection(string choiceHeader, Dictionary<string, string> choices) //GetUserSelection Class which will return a string and takes args choiceHeader and choices
         {
-            int choiceIdx;
-            bool isValidChoice = false;
-            string[] choiceKeys = new string[choices.Count];
+            int choiceIdx;                                                      //integer variable
+            bool isValidChoice = false;                                         //bool set to false
+            string[] choiceKeys = new string[choices.Count];                    //string array choiceKeys 
 
-            int i = 0;
-            foreach (KeyValuePair<string, string> choice in choices)
+            int i = 0;                                                          //set i to 0
+            foreach (KeyValuePair<string, string> choice in choices)            //iterate through each kvp in choices dict
             {
-                choiceKeys[i] = choice.Key;
-                i++;
+                choiceKeys[i] = choice.Key;                                         //??????Make list of choice keys???????
+                i++;                                                                
             }
 
-            do
-            {
-                Console.WriteLine("\n" + choiceHeader + " by:");
+            do                                                                  //start do-while loop
+            {                                                                   
+                Console.WriteLine("\n" + choiceHeader + " by:");                //print list heading using user choice
 
-                for (int j = 0; j < choiceKeys.Length; j++)
+                for (int j = 0; j < choiceKeys.Length; j++)                     //iterate from 0 to choiceKeys.Length by 1
                 {
-                    Console.WriteLine(j + " - " + choices[choiceKeys[j]]);
+                    Console.WriteLine(j + " - " + choices[choiceKeys[j]]);          //Print number of choice + "-" + the choice option
                 }
 
-                string input = Console.ReadLine();
-                choiceIdx = int.Parse(input);
+                string input = Console.ReadLine();                              //create input string from user input
+                choiceIdx = int.Parse(input);                                   //converts input string to int 
 
-                if (choiceIdx < 0 || choiceIdx >= choiceKeys.Length)
+                if (choiceIdx < 0 || choiceIdx >= choiceKeys.Length)            //throw error if they choose a number not on the list
                 {
-                    Console.WriteLine("Invalid choices. Try again.");
+                    Console.WriteLine("Invalid choices. Try again.");               //print this error
                 }
                 else
                 {
-                    isValidChoice = true;
+                    isValidChoice = true;                                           //if choice is valid then set bool to True
                 }
 
-            } while (!isValidChoice);
+            } while (!isValidChoice);                                           //rerun the method as long as false.  Ends do-while.
 
-            return choiceKeys[choiceIdx];
+            return choiceKeys[choiceIdx];                                       //return the users choice    
         }
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("printJobs is not implemented yet");
+            if (someJobs.Any() == false)
+            {
+                Console.WriteLine("There are no jobs fitting this description.");
+            }
+
+            else
+            {
+
+                //for each dict in the list...foreach (Dictionary<string, string> job in AllJobs) 
+                foreach (Dictionary<string, string> job in someJobs)
+                {
+                    Console.WriteLine("*****");
+                    //for each KVP in the dict
+                    foreach (KeyValuePair<string, string> criteria in job)
+                    {
+                        //Print key: value
+                        Console.WriteLine("{0}: {1}", criteria.Key, criteria.Value);
+                    }
+                    Console.WriteLine("*****");
+                }
+            }
+
+
         }
     }
 }
